d5c7347c7bf4f08df2ad3aca2f2b500813500f52
KonradSadowski <darnokowsky@gmail.com>
2023-04-01 13:59:10 +0200

    skończone zadanie 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEL = document.querySelector('.gallery');
+// creating img's
+for (const image of galleryItems) {
+  const imgEl = `<a class="gallery__item" href=${image.original}>
+  <img class="gallery__image" src=${image.preview} alt=${image.description} />
+</a>`;
+
+  galleryEL.insertAdjacentHTML('afterbegin', imgEl);
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+const imgClickHandler = event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+};
+galleryEL.addEventListener('click', imgClickHandler);
