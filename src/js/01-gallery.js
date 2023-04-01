7d99cb21e156b564bbe80f46cc4acf8bc8084811
KonradSadowski <darnokowsky@gmail.com>
2023-04-01 12:48:00 +0200

    zadanie 1 zakończone

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const galleryEl = document.querySelector(`.gallery`);
+const galleryEL = document.querySelector('.gallery');
+// creating img's
 for (const image of galleryItems) {
   const imgEl = `<div class="gallery__item">
-    <a class="gallery__link" href=${image.original}>
-    <img 
-    class="gallery__image"
-    src=${image.preview}
-    data-source=${image.original}
-    alt=${image.description}/>
-    </a>
-    </div>`;
+  <a class="gallery__link" href=${image.original}>
+  <img
+  class="gallery__image"
+  src=${image.preview}
+  data-source=${image.original}
+  alt=${image.description}/>
+  </a>
+  </div>`;
 
-  galleryEl.insertAdjacentHTML('afterbegin', imgEl);
-
-  galleryEl.addEventListener('click', event => event.preventDefault());
-
-  galleryEl.querySelector('.gallery__item').onclick = () => {
-    basicLightbox
-      .create(
-        `
-          <img src=${image.original}>
-      `
-      )
-      .show();
-  };
+  galleryEL.insertAdjacentHTML('afterbegin', imgEl);
 }
+
+const imgClickHandler = event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" >`);
+  instance.show();
+  document.addEventListener('keydown', event => {
+    if (event.code === 'Escape' || event.keyCode === 27) {
+      instance.close();
+    }
+  });
+};
+
+galleryEL.addEventListener('click', imgClickHandler);
